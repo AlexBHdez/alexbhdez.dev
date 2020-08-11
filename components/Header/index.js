5a838db9a719d83495415c3c8c40753bcca1e1b1
alexbhdez <abhbretones@gmail.com>
2020-08-11 20:24:39 +0200

    Add header and layout components

---
+++ components/Header/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Header = () => (
+  <header>
+    <div>
+      <Link href="/">
+        <a>Logo</a>
+      </Link>
+    </div>
+  </header>
+)
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+
 const Home = () => (
   <>
     <Head>
       <title>AlexBHdez.dev | Personal portfolio of a frontend developer</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
+
+    <Layout>
+      <Header />
+    </Layout>
   </>
 )
 
